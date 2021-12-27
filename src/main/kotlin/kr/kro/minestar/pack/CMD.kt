a558ef4ecfbfb07c76310a89ebd12f421d412745
MineStar <band1019@naver.com>
2021-12-28 01:20:21 +0900

    # arg 를 list 방식에서 enum 방식으로 변경

--- src/main/kotlin/kr/kro/minestar/pack/CMD.kt
+++ src/main/kotlin/kr/kro/minestar/pack/CMD.kt
@@ -8,28 +8,31 @@ import org.bukkit.command.TabCompleter
 import org.bukkit.entity.Player
 
 object CMD : CommandExecutor, TabCompleter {
-    private val args0 = listOf("cmd1", "cmd2", "cmd3")
+    private enum class Arg { cmd1, cmd2, cmd3 }
+
     override fun onCommand(player: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
         if (player !is Player) return false
         if (args.isEmpty()) "CMD".toPlayer(player).also { return false }
         when (args[0]) {
-            args0[0] -> {}
-            args0[1] -> {}
-            args0[2] -> {}
+            Arg.cmd1.name -> {}
+            Arg.cmd2.name -> {}
+            Arg.cmd3.name -> {}
         }
         return false
     }
 
     override fun onTabComplete(p: CommandSender, cmd: Command, alias: String, args: Array<out String>): MutableList<String> {
         val list = mutableListOf<String>()
-        val endNumber = args.size - 1
-        if (args.size == 1) for (s in args0) if (s.contains(args[endNumber])) list.add(s)
+        val arg = mutableListOf<String>()
+        if (args.size == 1) {
+            for (v in enumValues<Arg>()) arg.add(v.name)
+            for (s in arg) if (s.contains(args.last())) list.add(s)
+        }
         if (args.size > 1) when (args[0]) {
-            args0[0] -> {}
-            args0[1] -> {}
-            args0[2] -> {}
+            Arg.cmd1.name -> {}
+            Arg.cmd2.name -> {}
+            Arg.cmd3.name -> {}
         }
         return list
     }
-
 }
